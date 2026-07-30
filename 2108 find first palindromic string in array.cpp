93157fea0ebfb8bc2c766e0cc1cc07d5b2ad1c32
LOKESH <prajapatilokesh158@gmail.com>
2026-07-30 10:44:31 +0530

    Implement function to find first palindromic string

---
+++ 2108 find first palindromic string in array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    bool checkpalindrome(string s){
+        int l=0;
+        int r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(string i:words){
+            if(checkpalindrome(i)) return i;
+        }
+        return "";
+    }
